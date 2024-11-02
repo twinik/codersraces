ad28a5c0e01c3a8dd0ed2a000664ea37162a80ef
twinik <tobiwinik@gmail.com>
2024-11-01 23:15:53 -0300

    add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from "next/font/google";
 import { MainNav } from "@/components/main-nav";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
 						{children}
 					</div>
 				</div>
+				<SpeedInsights />
 			</body>
 		</html>
 	);
