463f8e925f8bd1368e42ea8d766c4fd835dd5fdb
twinik <tobiwinik@gmail.com>
2024-11-01 23:56:52 -0300

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import { MainNav } from "@/components/main-nav";
 
@@ -33,6 +34,7 @@ export default function RootLayout({
 					</div>
 				</div>
 				<SpeedInsights />
+				<Analytics />
 			</body>
 		</html>
 	);
